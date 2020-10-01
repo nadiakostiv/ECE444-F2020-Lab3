0555ab28b1d21c1cd5ece51614c17afaad6e6f99
nadiakostiv <nadiakostiv98@gmail.com>
2020-10-01 16:11:38 -0400

    Example 4-7 from textbooks, and adding the email field

--- hello.py
+++ hello.py
@@ -1,15 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required, ValidationError
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'rlly rlly hard to guess string'
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+def isEmail(form, field):
+        if '@' not in field.data:
+                raise ValidationError("Please include an '@' in the email address. %s is missing an '@'." %(field.data))
+
+class NameForm(Form):
+	name = StringField('What is your name?', validators=[Required()])
+	email = StringField('What is your UofT email address?', validators=[Required(), isEmail])
+	submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html', current_time=datetime.utcnow())
+	form = NameForm()
+	if form.validate_on_submit():
+		old_name = session.get('name')
+		old_email = session.get('email')
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you changed your name!')
+		if old_email is not None and old_email != form.email.data:
+                        flash('Looks like you changed your email!')
+		session['name'] = form.name.data
+		session['email'] = form.email.data
+		return redirect(url_for('index'))
+	return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
@@ -18,10 +42,6 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-	return render_template('500.html'), 500
 
 if __name__ == '__main__':
 	app.run(debug=True)
